74a9adde8343a9d17115784b7d02d51b06b55096
eldarhasandwich <hasanovicel@gmail.com>
2017-05-18 22:17:00 +1000

    Completed All Else
    I had quite a bit of trouble conceptualizing declarative programming here at first but it became easier with the more examples I did. I seemed to solve the last question without using the reduce function.
    
    I had a lot of trouble solving the extra credit questions declaratively, opting for imperative statements.

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -21,18 +21,18 @@ describe("About Applying What We Have Learnt", function() {
     var i,j,hasMushrooms, productsICanEat = [];
 
     for (i = 0; i < products.length; i+=1) {
-        if (products[i].containsNuts === false) {
+        if (products[i].containsNuts === false) { // if there are no nuts
             hasMushrooms = false;
-            for (j = 0; j < products[i].ingredients.length; j+=1) {
+            for (j = 0; j < products[i].ingredients.length; j+=1) { // look for mushrooms
                if (products[i].ingredients[j] === "mushrooms") {
                   hasMushrooms = true;
-               }
+               } // we should break this loop if we find mushrooms
             }
-            if (!hasMushrooms) productsICanEat.push(products[i]);
+            if (!hasMushrooms) productsICanEat.push(products[i]); // if no mushrooms, add to products
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -41,7 +41,12 @@ describe("About Applying What We Have Learnt", function() {
 
       /* solve using filter() & all() / any() */
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      var hasMushroom = function(x) { return x === "mushrooms" } // true if x has mushrooms
+      var productsICanEat = _(products).filter(function (x) { return x.containsNuts === false
+            && !_(x.ingredients).any(hasMushroom) }); // if no nuts and no mushrooms
+
+
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
@@ -55,14 +60,16 @@ describe("About Applying What We Have Learnt", function() {
       }
     }
 
-    expect(sum).toBe(FILL_ME_IN);
+    expect(sum).toBe(233168);
   });
 
   it("should add all the natural numbers below 1000 that are multiples of 3 or 5 (functional)", function () {
 
-    var sum = FILL_ME_IN;    /* try chaining range() and reduce() */
+    /* try chaining range() and reduce() */
+    var sum = _( _.range(0,1000) ).reduce(
+          function(sum, x) {if (x%3===0 || x%5===0) {return sum + x} else {return sum}} );
 
-    expect(233168).toBe(FILL_ME_IN);
+    expect(233168).toBe(sum);
   });
 
   /*********************************************************************************/
@@ -75,39 +82,112 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   it("should count the ingredient occurrence (functional)", function () {
     var ingredientCount = { "{ingredient name}": 0 };
 
     /* chain() together map(), flatten() and reduce() */
+    // turn everything into its ingredient array, flatten it, and summate ingredients
+    var addToHistogram = function (x) {
+      ingredientCount[x] = (ingredientCount[x] || 0) + 1; }
 
-    expect(ingredientCount['mushrooms']).toBe(FILL_ME_IN);
+    _(products).chain() .map(function(x) { return x.ingredients })
+                      .flatten()
+                      .forEach(addToHistogram);
+
+    // I didn't see how this could be achived with reduce? I used forEach()
+    // Same result as imperative if you look at the full ingredientCount dictionary
+
+    expect(ingredientCount['mushrooms']).toBe(2);
   });
 
   /*********************************************************************************/
   /* UNCOMMENT FOR EXTRA CREDIT */
-  /*
+
   it("should find the largest prime factor of a composite number", function () {
 
   });
 
   it("should find the largest palindrome made from the product of two 3 digit numbers", function () {
 
+    var aPointer = 100;
+    var bPointer = 100;
+    var largestPalindrome = 0;
+    // number.length
+    function productIsPalindrome(a,b) {
+      var item = (a*b).toString().split("");
+      var counter = 0;
+      while (counter < item.length / 2) {
+        if (item[counter] != item[item.length-1-counter]) {
+          return false;
+        }
+        counter ++;
+      }
+      return true;
+    }
+
+    for(var a = 100; a < 1001; a++) {
+      for(var b = 100; b < 1001; b++) {
+        if (productIsPalindrome(a,b)) {
+          if ((a*b) > largestPalindrome) {
+            aPointer = a;
+            bPointer = b;
+            largestPalindrome = (a*b);
+          }
+        }
+      }
+    }
+
+    expect(largestPalindrome).toBe(906609);
+
   });
 
   it("should find the smallest number divisible by each of the numbers 1 to 20", function () {
+    divisibleRange = _.range(1,21);
+    function isDivisible(num) {
+      for(var i = 0; i < divisibleRange.length; i++) {
+        if (num % divisibleRange[i] != 0) {
+          return false;
+        }
+      }
+      return true;
+    }
 
+    smallestNum = 20;
+    flag = false;
+    while (flag == false) {
+      smallestNum ++;
+      if (isDivisible(smallestNum)) {
+        flag = true;
+      }
+    }
+
+    expect(smallestNum).toBe(232792560);
 
   });
 
   it("should find the difference between the sum of the squares and the square of the sums", function () {
+
+    var n = 10;
+
+    var nRange = _.range(1, n+1);
+    var sumOfSquares = _(nRange).reduce(
+        function(sum, x) {return (sum + Math.pow(x,2))}, 0); //something is wrong here
+    var squareOfSums = Math.pow(_(nRange).reduce(
+        function(sum, x) {return sum + x}, 0),2);
+
+    var difference = sumOfSquares - squareOfSums;
+
+    // this can be solved with (n-1)n(n+1)(3n+2) /12 in constant time
+
+    expect(difference).toBe(-2640);
 
   });
 
   it("should find the 10001st prime", function () {
 
   });
-  */
+
 });
